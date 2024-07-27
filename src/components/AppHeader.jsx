e62acd436371ea1acf66e6302b5f6b498308170d
Arshad008 <arshadrauff008@gmail.com>
2024-07-27 11:13:32 +0530

    style changes

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,11 +3,15 @@ import { AppBar, Collapse, LinearProgress, Toolbar, Typography } from "@mui/mate
 
 import { StoreContext } from '../store';
 
+const containerStyles = {
+  background: 'radial-gradient(farthest-corner circle at 0% 0%, hsla(210, 100%, 23%, 0.2) 0%, hsl(210, 14%, 7%) 100%)'
+};
+
 const AppHeader = () => {
   const { store, setStore } = useContext(StoreContext);
 
   return (
-    <AppBar>
+    <AppBar sx={containerStyles}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Gem Classifier
