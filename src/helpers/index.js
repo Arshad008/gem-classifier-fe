2bd070d005014ebf1a8e76c294de4f4684aed2af
Arshad008 <arshadrauff008@gmail.com>
2024-07-27 14:12:32 +0530

    email validation

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -29,3 +29,11 @@ export const getConvertedJpgFile = (file) => {
     reader.readAsDataURL(file);
   });
 };
+
+export const isEmailValid = (email = "") => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
